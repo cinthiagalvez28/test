695d7e2e3c7d4516008ca8f1eaf76ee0d3a90d4c
RukiaGal28 <cinthia_galvez28@hotmail.com>
2021-09-24 17:29:36 -0500

    Updated code.

--- pom/pages/DashboardPage.js
+++ pom/pages/DashboardPage.js
@@ -3,11 +3,11 @@ import { Selector } from 'testcafe'
 class DashboardPage {
   constructor(){
     this.entradaButton = Selector('div').withText('Registrar entrada')
-    this.salidaButton = Selector('div[data-v-8733fc98]')
-    this.seguroButton = Selector('span[data-v-8733fc98]')
     this.entradaRegistradaTitle = Selector('h1').withText('Entrada registrada')
-    this.salidaRegistradaTitle = Selector('h1[data-v-534a445f]')
     this.saludoTitle = Selector('h1').withText('¡Hola, Cinthia Alejandra!')
+    this.salidaButton = Selector('div[data-v-8733fc98]')
+    this.seguroButton = Selector('button[type="submit"]')
+    this.salidaRegistradaTitle = Selector('h1[data-v-534a445f]')
   }
 }
 export default new DashboardPage()
