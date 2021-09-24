9aac77a554fd190bbc9525b9a6a5b513cd86b360
RukiaGal28 <cinthia_galvez28@hotmail.com>
2021-09-24 16:51:15 -0500

    Updated selectors.

--- pom/pages/DashboardPage.js
+++ pom/pages/DashboardPage.js
@@ -3,10 +3,10 @@ import { Selector } from 'testcafe'
 class DashboardPage {
   constructor(){
     this.entradaButton = Selector('div').withText('Registrar entrada')
-    this.salidaButton = Selector('div').withText('Registrar salida')
-    this.seguroButton = Selector('span').withText('Sí, seguro')
+    this.salidaButton = Selector('div[data-v-8733fc98]')
+    this.seguroButton = Selector('span[data-v-8733fc98]')
     this.entradaRegistradaTitle = Selector('h1').withText('Entrada registrada')
-    this.salidaRegistradaTitle = Selector('h1').withText('Salida registrada')
+    this.salidaRegistradaTitle = Selector('h1[data-v-534a445f]')
     this.saludoTitle = Selector('h1').withText('¡Hola, Cinthia Alejandra!')
   }
 }
