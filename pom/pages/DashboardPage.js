32c05e7e5ceb99671ce52fb059b206442ca8e797
RukiaGal28 <cinthia_galvez28@hotmail.com>
2021-09-27 14:02:36 -0500

    Deleted unnecessary selector data.

--- pom/pages/DashboardPage.js
+++ pom/pages/DashboardPage.js
@@ -2,9 +2,8 @@ import { Selector } from 'testcafe'
 
 class DashboardPage {
   constructor(){
-    this.entradaButton = Selector('div').withText('Registrar entrada')
-    this.entradaRegistradaTitle = Selector('h1').withText('Entrada registrada')
-    this.saludoTitle = Selector('h1').withText('¡Hola, Cinthia Alejandra!')
+    this.entradaButton = Selector('div')
+    this.entradaRegistradaTitle = Selector('h1')
     this.salidaButton = Selector('div[data-v-8733fc98]')
     this.seguroButton = Selector('button[type="submit"]')
     this.salidaRegistradaTitle = Selector('h1[data-v-534a445f]')
