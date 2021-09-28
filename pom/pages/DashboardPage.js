ab784bf4da9cb93b3fff14fdea91f65ab94c0dde
RukiaGal28 <cinthia_galvez28@hotmail.com>
2021-09-28 09:16:43 -0500

    Update DashboardPage.js

--- pom/pages/DashboardPage.js
+++ pom/pages/DashboardPage.js
@@ -3,7 +3,6 @@ import { Selector } from 'testcafe'
 class DashboardPage {
   constructor(){
     this.entradaButton = Selector('div[data-v-8733fc98]').withText('ENTRADA')
-    this.entradaRegistradaTitle = Selector('h1[data-v-534a445f]')
     this.salidaButton = Selector('div[data-v-8733fc98]').withText('SALIDA')
     this.seguroButton = Selector('button[type="submit"]')
     this.registradoTitle = Selector('h1[data-v-534a445f]')
