e1c4b00756c8632fc909077eb4df14487063c766
RukiaGal28 <cinthia_galvez28@hotmail.com>
2021-09-29 17:52:38 -0500

    Update CheckOut.test.js

--- pom/tests/CheckOut.test.js
+++ pom/tests/CheckOut.test.js
@@ -2,7 +2,7 @@ import loginPage from '../pages/LoginPage.js'
 import dashboardPage from '../pages/DashboardPage.js'
 import { URLS, CREDENTIALS } from '../data/Constants.js'
 
-fixture('Check In feature testing user')
+fixture('Check Out feature testing user')
   .page`${URLS.LOGIN_URL}`
 
 test('As a standard user, I should be able to log in and check out when I provide valid credentials', async t =>{
